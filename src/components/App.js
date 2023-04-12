34da2293bb5769708ece5eb21785174ba7ad3fdf
Taufique Raja <80124042+TaufiqueRaja@users.noreply.github.com>
2023-04-13 01:01:58 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { dec, inc, incByNum, signIn, signOut } from '../actions/action';
 
 function App() {
-
+  const {counter,islogged}=useSelector(state=>state);
+  const [inputValue,setInputValue] = useState(2);
+  const dispatch = useDispatch()
+  console.log(counter);
   return (
     <div id='main'>
-      
+      {islogged && <div data-testid='counter'>{counter}</div>}
+      {islogged? <button onClick={()=>{dispatch(signOut())}}>Logout</button>:<button onClick={()=>{dispatch(signIn())}}>Login</button>}
+      <input type="number"  onChange={e=>{setInputValue(Number(e.target.value))}}/>
+      <button onClick={()=>{dispatch(incByNum(inputValue))}}>Add amount</button>
+      <button onClick={()=>{dispatch(inc())}}>+</button>
+      <button onClick={()=>{dispatch(dec())}}>-</button>
     </div>
   );
 }
